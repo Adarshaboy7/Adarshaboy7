0e2b0c07acf21d237f914856bf929bfd5c06eadc
Adarshaboy7 <136305623+Adarshaboy7@users.noreply.github.com>
2023-06-14 00:25:24 +0530

    operators.js

---
+++ operators.js
@@ -0,0 +1,54 @@
+console.log("operators in js")
+let a=2;
+let b=8;
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a*b=",a*b)
+console.log("a/b=",a/b)
+console.log("a%b=",a%b)
+console.log("a**b=",a**b)
+console.log("++a=",++a)
+console.log("a--=",a--)
+//Assignment operators
+let assignment=1;
+assignment +=5//same as assignment=assignment +5
+console.log(assignment)
+assignment -=5//same as assignment=assignment -5
+console.log(assignment)
+assignment *=5//same as assignment=assignment *5
+console.log(assignment)
+assignment /=5//same as assignment=assignment /5
+console.log(assignment)
+assignment %=5//same as assignment=assignment %5
+console.log(assignment)
+assignment **=5//same as assignment=assignment **5
+console.log(assignment)
+//comparison operators
+let comp1=9
+let comp2=6
+console.log("comp1==comp2 is",comp1==comp2)
+console.log("comp1==comp2 is",comp1!=comp2)
+console.log("comp1===comp2 is",comp1===comp2)
+console.log("comp1!==comp2 is",comp1!==comp2)
+console.log("comp1<comp2 is",comp1<comp2)
+console.log("comp1>comp2 is",comp1>comp2)
+console.log("comp1<=comp2 is",comp1<=comp2)
+console.log("comp1>=comp2 is",comp1>=comp2)
+let comp3=6
+let comp4=6
+console.log("comp3==comp4 is",comp3==comp4)
+console.log("comp3!=comp4 is",comp3!=comp4)
+console.log("comp3===comp4 is",comp3===comp4)
+console.log("comp3!==comp4 is",comp3!==comp4)
+let comp5=6
+let comp6="6"
+console.log("comp5==comp6 is",comp5==comp6)
+console.log("comp5!=comp6 is",comp5!=comp6)
+console.log("comp5===comp6 is",comp5===comp6)
+console.log("comp5!==comp6 is",comp5!==comp6)
+//logical operators
+let x=4;
+let y=7;
+console.log(x<y&&x==4)
+console.log(x<y||x==4)
+console.log(!Tr)
