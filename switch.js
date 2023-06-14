9cb8d068b6f446438af1d7561f12cb13ddab2639
Adarsha koley <136305623+Adarshaboy7@users.noreply.github.com>
2023-06-14 10:57:04 +0530

    switch.js

---
+++ switch.js
@@ -0,0 +1,24 @@
+let age= prompt(" What is your age?")
+switch(age){
+  case '10':
+    console.log("you age is 10");
+    break
+  case '12':
+    console.log("you age is 12");
+    break
+  case '14':
+    console.log("you age is 14");
+    break
+  case '16':
+    console.log("you age is 16");
+    break
+  case '18':
+    console.log("you age is 18");
+    break
+  case '20':
+    console.log("you age is 20");
+    break
+  default:
+    console.log("you age is no special")
+}
+    
