12e5db66aba90820fd114d04148176121820700d
Adarsha koley <136305623+Adarshaboy7@users.noreply.github.com>
2023-06-14 21:01:14 +0530

    Create Function.js

---
+++ Function.js
@@ -0,0 +1,14 @@
+function oneplueAvg(x,y){
+  return  (1+(x+y)/2)
+}
+let a=1;
+let b=2;
+let c=3;
+console.log("one plue Avgerage of a and b is",oneplueAvg(a,b))
+console.log("one plue Avgerage of b and c is",oneplueAvg(b,c))
+console.log("one plue Avgerage of a and c is",oneplueAvg(a,c))
+//New 
+const sum=(p,q)=>{
+  return p+q
+}
+console.log(sum(9,7))
