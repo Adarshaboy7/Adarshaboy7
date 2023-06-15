89309f316e3cbef7c4b3b4a5ea5a341c2eb4524c
Adarsha koley <136305623+Adarshaboy7@users.noreply.github.com>
2023-06-15 12:43:54 +0530

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,17 @@
+//let name="Adarsha"
+//console.log(name.lenght)
+console.log(name[0])
+console.log(name[1])
+let friend="Disha"
+console.log(friend)
+//let myfriend="pappu")//dont do this
+//console.log(myfriend)
+//Template literals
+let boy1="pramod"
+let boy2="nikhil"
+//nikil is a friend of pramod
+let setence='boy1 is a friend of boy2'
+console.log(sentence)
+//Escape sequce characters
+let fruit='Bana\na'
+console.log(fruit)
