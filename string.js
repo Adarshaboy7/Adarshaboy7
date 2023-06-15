54aec14b6d7ff75cbc1dcfcc4cb0083f14d3fe26
Adarsha koley <136305623+Adarshaboy7@users.noreply.github.com>
2023-06-16 02:21:34 +0530

    string.js

---
+++ string.js
@@ -0,0 +1,11 @@
+let name="Adarsha"
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,4))
+console.log(name.slice(2))
+console.log(name.replace("ada","ama"))
+let friend="pappu"
+console.log(name .concat (" is a friend of" , friend, "ok"))
+let friend2="      Disha      "
+console.log(friend2.trim())
