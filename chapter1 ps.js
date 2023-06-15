f5473e88a66d135c4e1217d0fe84b7ca784fdffd
Adarsha koley <136305623+Adarshaboy7@users.noreply.github.com>
2023-06-16 00:13:26 +0530

    chapter1 ps.js

---
+++ chapter1 ps.js
@@ -0,0 +1,23 @@
+//chapter 1-Q1
+let a="Adarsha"
+let b= 8
+console.log(a+b)
+//chapter 1-Q2
+console.log(typeof(a+b))
+//chapter 1-Q3
+const a1={
+  name:"Adarsha",
+  section:1,
+  isprincipal:false
+}
+a1="Adarsha"
+//chapter 1-Q4
+a1['friend']="pappu"
+a1['name']="roni"
+cosole.log(a1)
+//chapter 1-Q5
+const dict={
+  appreciate:"reconize the full wort of",
+  ataraxia:"a state of freedom from emotional disturbance and anxiety",
+}
+console.log(dict[appreciate])
