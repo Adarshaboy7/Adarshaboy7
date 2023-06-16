e0c300152b45acc4aafa092b85756c132a6123d0
Adarsha koley <136305623+Adarshaboy7@users.noreply.github.com>
2023-06-16 15:14:43 +0530

    Array methodes.js

---
+++ Array methodes.js
@@ -0,0 +1,14 @@
+//Array methodes
+let num=[1,2,3,34,4]
+let b=num.toString()//b is now  a string
+console.log(b,typeof b)
+let c=num.join("and")
+console.log(c,typeof c)
+let d=num.pop()//pop returns the poped element
+console.log(num,d)
+let e=num.push(56)//push returns the new array length
+console.log(num,e)
+let f=num.shift()//removes  an element from the start of the array
+console.log(num,f)
+let g=num.unshift(78)//removes  an element from the start of the array
+console.log(num,g)
