bfc7e4f555ddfcf5a906302737473f85442860ff
Adarsha koley <136305623+Adarshaboy7@users.noreply.github.com>
2023-06-16 14:19:18 +0530

    Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,14 @@
+let marks_class_12=[43,53,65,75,false,"not present"]
+console.log((marks_class_12[0]))
+console.log((marks_class_12[1]))
+console.log((marks_class_12[2]))
+console.log((marks_class_12[3]))
+console.log((marks_class_12[4]))
+console.log((marks_class_12[5]))
+console.log((marks_class_12[6]))//will be undefined because index 6 does not exist
+console.log("the lenght of marks_class_12 is",marks_class_12.length)
+marks_class_12[6]=89//Adding a new  value to the array
+marks_class_12[0]=96//Adding a new value to the array
+console.log(marks_class_12)
+console.log(marks_class_12)
+console.log(typeof marks_class_12 )
