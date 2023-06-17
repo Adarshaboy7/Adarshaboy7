9d51442baaeb1f0090e3cca3fb89806609c2091b
Adarsha koley <136305623+Adarshaboy7@users.noreply.github.com>
2023-06-17 16:07:58 +0530

    types of console.js

---
+++ types of console.js
@@ -0,0 +1,11 @@
+console.log("log")
+console.info("info")
+console.warn("warn")
+console.error("err")
+console.assert("err" !=false)
+console.assert("err" ==false)
+console.time("forLoop")
+for(let i=0;i<5;i++){
+  console.log(223)
+}
+console.timeEnd("forLoop")
