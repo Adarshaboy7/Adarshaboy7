023a1f87cffe8639631b9c98607c7d83c4d8ffb5
Adarsha koley <136305623+Adarshaboy7@users.noreply.github.com>
2024-02-06 01:03:19 +0530

    call by value.c

---
+++ call by value.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// Function to swap two variables using call by value
+void swapByValue(int a, int b) {
+int temp = a;
+a = b;
+b = temp;
+}
+void main() {
+int num1 = 5, num2 = 10;
+printf("Before swap (call by value): num1 = %d, num2 = %d\n", num1, num2);
+// Call the function with call by value
+swapByValue(num1, num2);
+printf("After swap (call by value): num1 = %d, num2 = %d\n", num1, num2);
+}
