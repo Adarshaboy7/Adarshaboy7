8b8fbb13a9594bb4210a8ce250c339d47324ecae
Adarsha koley <136305623+Adarshaboy7@users.noreply.github.com>
2024-02-06 00:07:42 +0530

    Singly Linked List after a desired node and display the list.c

---
+++ Singly Linked List after a desired node and display the list.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <malloc.h>
+struct node
+{
+int data;
+struct node *next;
+};
+struct node *start = NULL;
+struct node *create_ll(struct node *);
+struct node *display(struct node *);
+struct node *insert_after(struct node *);
+int main()
+{
+
+int option;
+do
+{
+printf("\n *****MAIN MENU *****\n");
+printf("\n 1: Create a list");
+printf("\n 2: Display the list");
+printf("\n 3: Add a node after a given node");
+printf("\n 4: EXIT");
+printf("\n\n Enter your option : ");
+scanf("%d", &option);
+switch(option)
+{
+case 1: start = create_ll(start);
+printf("\n LINKED LIST CREATED");
+break;
+case 2: start = display(start);
+break;
+case 3: start = insert_after(start);
+break;
+}
+}while(option !=4);
+getch();
+return 0;
+}
+struct node *create_ll(struct node *start)
+{
+struct node *new_node, *ptr;
+int num;
+printf("\n Enter -1 to end");
+printf("\n Enter the data : ");
+scanf("%d", &num);
+while(num!=-1)
+{
+new_node = (struct node*)malloc(sizeof(struct node));
+new_node -> data=num;
+if(start==NULL)
+{
+new_node -> next = NULL;
+start = new_node;
+}
+else
+{
+ptr=start;
+while(ptr->next!=NULL)
+
+ptr=ptr->next;
+ptr->next = new_node;
+new_node->next=NULL;
+}
+printf("\n Enter the data : ");
+scanf("%d", &num);
+}
+return start;
+}
+struct node *display(struct node *start)
+{
+struct node *ptr;
+ptr = start;
+while(ptr != NULL)
+{
+printf("\t %d", ptr -> data);
+ptr = ptr -> next;
+}
+return start;
+}
+struct node *insert_after(struct node *start)
+{
+struct node *new_node, *ptr, *preptr;
+int num, val;
+printf("\n Enter the data : ");
+scanf("%d", &num);
+printf("\n Enter the value after which the data has to be inserted : ");
+scanf("%d", &val);
+new_node = (struct node *)malloc(sizeof(struct node));
+new_node -> data = num;
+ptr = start;
+preptr = ptr;
+while(preptr -> data != val)
+{
+preptr = ptr;
+ptr = ptr -> next;
+}
+preptr -> next=new_node;
+new_node -> next = ptr;
+return start;
+}
